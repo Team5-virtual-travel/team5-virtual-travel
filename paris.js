0baee4ebf9895b8744d9f2e59ddb0627a8f3352a
tuvaelises <tuva.akerlund@hyperisland.se>
2020-11-27 14:37:29 +0100

    First push

--- paris.js
+++ paris.js
@@ -1,1 +1,9 @@
+const apiKey = "e67cf100b2dea20b1cb1d453505d5859";
+const url = "https://api.openweathermap.org/data/2.5/weather";
+const city = "Paris";
+const unit = "metric";
 
+const requestURL = url.concat("?q",city,"&appid=",apiKey,"&units=",unit)
+fetch(requestURL)
+  .then(response => response.json())
+  .then(data => console.log(data));
